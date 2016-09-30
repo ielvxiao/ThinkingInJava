b554cffbde6daff63e49504a2a08153131ba9ff6
lvxiao <ielvxiao@gmail.com>
2016-09-30 16:49:54 +0800

    修改格式

--- src/staticpractice/StaticInitialization.java
+++ src/staticpractice/StaticInitialization.java
@@ -5,7 +5,11 @@ import net.sf.json.JSONArray;
 /**
  * Created by Administrator on 2016/9/20.
  * Thinking in java 5.7.2
+ * 要执行main方法首先要要加载staticpractice类，然后其静态域table和cupboard被初始化，这将导致它们对应的类也被加载，并且由于它们也都是包含静态的BOWL对象，因此BOWL也被
+ 加载。这样，在这个特殊的程序中的所有类在MAIN（）开始之前都被加载了。
  */
+
+
 public class StaticInitialization {
     public static void main(String[] args){
          System.out.println("Creating new Cupboard() in main");
@@ -18,7 +22,4 @@ public class StaticInitialization {
     static Table table=new Table();
     static Cupboard cupboard=new Cupboard();
 }
-/*
-要执行main方法首先要要加载staticpractice类，然后其静态域table和cupboard被初始化，这将导致它们对应的类也被加载，并且由于它们也都是包含静态的BOWL对象，因此BOWL也被
-加载。这样，在这个特殊的程序中的所有类在MAIN（）开始之前都被加载了。
- */
+
