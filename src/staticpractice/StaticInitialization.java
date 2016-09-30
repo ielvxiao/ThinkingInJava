373568f2d9f790791328962f528bdcf2858b1f29
lvxiao <ielvxiao@gmail.com>
2016-09-30 16:47:51 +0800

    再main方法中添加了注释。

--- src/staticpractice/StaticInitialization.java
+++ src/staticpractice/StaticInitialization.java
@@ -18,3 +18,7 @@ public class StaticInitialization {
     static Table table=new Table();
     static Cupboard cupboard=new Cupboard();
 }
+/*
+要执行main方法首先要要加载staticpractice类，然后其静态域table和cupboard被初始化，这将导致它们对应的类也被加载，并且由于它们也都是包含静态的BOWL对象，因此BOWL也被
+加载。这样，在这个特殊的程序中的所有类在MAIN（）开始之前都被加载了。
+ */
