9d9703b584304f9c0f4ee65545cba66d3ba0d893
LvXiao <ielvxiao@gmail.com>
2017-03-08 16:33:15 +0800

    count的Increment和value方法都是synchronized的，camceled是一个volatile布尔值。可以安全的操作

---
+++ src/thread/atomicclass/OrnamentalGarden.java
@@ -0,0 +1,100 @@
+package thread.atomicclass;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Administrator
+ * @date 2017/3/8
+ */
+class Count {
+    private int count = 0;
+    private Random rand = new Random(47);
+    public synchronized int increment() {
+        int temp = count;
+        if (rand.nextBoolean())
+            Thread.yield();
+        return (count = ++temp);
+    }
+
+    public synchronized int value() {
+        return count;
+    }
+}
+
+class Entrance implements Runnable {
+    private static Count count = new Count();
+    private static List<Entrance> entrances =
+            new ArrayList<>();
+    private int number = 0;
+    //不需要synchronization
+    private final int id;
+    private static volatile boolean canceled = false;
+    //原子操作不需要volatitle
+    public static void cancel(){
+        canceled = true;
+    }
+
+    public Entrance(int id) {
+        this.id = id;
+        entrances.add(this);
+    }
+
+    @Override
+    public void run() {
+        while (!canceled) {
+            synchronized (this) {
+                ++ number;
+            }
+            System.out.println(this + " Totaleeeee: " + count.increment());
+            try {
+                TimeUnit.MILLISECONDS.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println("sleep interrupted");
+            }
+        }
+        System.out.println("Stopping " + this);
+    }
+
+    public synchronized int getValue() {return number;}
+
+    @Override
+    public String toString() {
+        return "Entranceeeee " + id + " : " +getValue();
+    }
+
+    public static int getTotalCount() {
+        return count.value();
+    }
+
+    public static int sumEntrances() {
+        int sum = 0;
+        for (Entrance entrance: entrances
+             ) {
+            sum += entrance.getValue();
+        }
+        return sum;
+    }
+}
+
+public class OrnamentalGarden {
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            exec.execute(new Entrance(i));
+        }
+        TimeUnit.SECONDS.sleep(3);
+        Entrance.cancel();
+        exec.shutdown();
+        //awaitTermination()方法等待每个任务结束，如果所有的任务在超时时间之前结束，返回true，否则返回false。
+        if (!exec.awaitTermination(250, TimeUnit.MILLISECONDS)){
+            System.out.println("Some tasks were not terminated!");
+        }
+        System.out.println("Total: " + Entrance.getTotalCount());
+        System.out.println("Sum of Entrances: " + Entrance.sumEntrances());
+    }
+}
