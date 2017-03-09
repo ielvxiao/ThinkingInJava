9d5381a11fa2ba3244cbbfa5c1341d2921323493
LvXiao <ielvxiao@gmail.com>
2017-03-09 16:40:49 +0800

    生产者与消费者队列。解决了多线程中高效安全传输数据的问题。在使用这些队列的时候我们不用担心什么时候需要阻塞线程，什么时候需要唤醒线程，队列都可以一手包办

--- src/thread/threadCooperation/Wax0Matic.java
+++ src/thread/threadCooperation/Wax0Matic.java
@@ -3,33 +3,88 @@ package thread.threadCooperation;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author Administrator
  * @date 2017/3/9
  */
 class Car {
+//    private boolean wax0n = false;
+//    public synchronized void waxed() {
+//        wax0n = true;
+//        notifyAll();
+//    }
+//    public synchronized void buffed() {
+//        wax0n = false;
+//        notifyAll();
+//    }
+//    public synchronized void waitForWaxing() throws InterruptedException {
+//        while (wax0n == false)
+//            wait();
+//    }
+//    public synchronized void waitForBuffing() throws InterruptedException {
+//        while (wax0n == true)
+//            wait();
+//    }
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
     private boolean wax0n = false;
-    public synchronized void waxed() {
-        wax0n = true;
-        notifyAll();
+
+    public void waxed() {
+        lock.lock();
+        try {
+            wax0n = true;
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
     }
     public synchronized void buffed() {
-        wax0n = false;
-        notifyAll();
+        lock.lock();
+        try {
+            wax0n = false;
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
     }
-    public synchronized void waitForWaxing() throws InterruptedException {
-        while (wax0n == false)
-            wait();
-    }
-    public synchronized void waitForBuffing() throws InterruptedException {
-        while (wax0n == true)
-            wait();
+
+    public void waitForWaxing() throws InterruptedException {
+        lock.lock();
+        try {
+            while (wax0n == false)
+                condition.await();
+        } finally {
+            lock.unlock();
+        }
     }
 }
 
 class Wax0n implements Runnable {
-    private Car car;
+//    private Car car;
+//
+//    public Wax0n(Car car) {
+//        this.car = car;
+//    }
+//
+//    @Override
+//    public void run() {
+//        try {
+//            while (!Thread.interrupted()) {
+//                System.out.println("Wax 0n!");
+//                TimeUnit.MILLISECONDS.sleep(200);
+//                car.waxed();
+//                car.waitForBuffing();
+//            }
+//        } catch (InterruptedException e) {
+//            System.out.println("Exiting via interrupt");
+//        }
+//        System.out.println("Ending Wax 0n task");
+//    }
+private Car car;
 
     public Wax0n(Car car) {
         this.car = car;
@@ -37,22 +92,42 @@ class Wax0n implements Runnable {
 
     @Override
     public void run() {
-    try {
-        while (!Thread.interrupted()) {
-            System.out.println("Wax 0n!");
-            TimeUnit.MILLISECONDS.sleep(200);
-            car.waxed();
-            car.waitForBuffing();
+        try {
+            while (!Thread.interrupted()) {
+                System.out.println("Wax on! ");
+                TimeUnit.MILLISECONDS.sleep(200);
+                car.waxed();
+                car.waitForWaxing();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Exiting via interrupt");
         }
-    } catch (InterruptedException e) {
-        System.out.println("Exiting via interrupt");
-    }
-        System.out.println("Ending Wax 0n task");
+        System.out.println("Ending Wax On task");
     }
 }
 
 class Wax0ff implements Runnable {
-    private Car car;
+//    private Car car;
+//
+//    public Wax0ff(Car car) {
+//        this.car = car;
+//    }
+//
+//    @Override
+//    public void run() {
+//        try {
+//            while (!Thread.interrupted()) {
+//                car.waitForWaxing();
+//                System.out.println("Wax 0ff!");
+//                TimeUnit.MILLISECONDS.sleep(200);
+//                car.buffed();
+//            }
+//        } catch (InterruptedException e) {
+//            System.out.println("Exiting via interrupt");
+//        }
+//        System.out.println("Ending wax 0ff task");
+//    }
+private Car car;
 
     public Wax0ff(Car car) {
         this.car = car;
@@ -75,12 +150,20 @@ class Wax0ff implements Runnable {
 }
 
 public class Wax0Matic {
-    public static void main(String[] args) throws Exception {
-        Car car = new Car();
-        ExecutorService exec = Executors.newCachedThreadPool();
-        exec.execute(new Wax0ff(car));
-        exec.execute(new Wax0n(car));
-        TimeUnit.SECONDS.sleep(1);
-        exec.shutdown();
-    }
+//    public static void main(String[] args) throws Exception {
+//        Car car = new Car();
+//        ExecutorService exec = Executors.newCachedThreadPool();
+//        exec.execute(new Wax0ff(car));
+//        exec.execute(new Wax0n(car));
+//        TimeUnit.SECONDS.sleep(1);
+//        exec.shutdown();
+//    }
+public static void main(String[] args) throws Exception {
+    Car car = new Car();
+    ExecutorService exec = Executors.newCachedThreadPool();
+    exec.execute(new Wax0ff(car));
+    exec.execute(new Wax0n(car));
+    TimeUnit.SECONDS.sleep(5);
+    exec.shutdown();
+}
 }
